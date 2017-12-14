dd8c81498d396b69487be253a51f0e4226a72fba
caitinggui <1029645297@qq.com>
2017-12-14 19:03:02 +0800

    join and in are OK

---
+++ database/183_customers_who_never_order.sql
@@ -0,0 +1,44 @@
+-- Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+-- Table: Customers.
+
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+
+-- Table: Orders.
+
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+
+-- Using the above tables as example, return the following:
+
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+Create table If Not Exists Customers (Id int, Name varchar(255));
+Create table If Not Exists Orders (Id int, CustomerId int);
+Truncate table Customers;
+insert into Customers (Id, Name) values ('1', 'Joe');
+insert into Customers (Id, Name) values ('2', 'Henry');
+insert into Customers (Id, Name) values ('3', 'Sam');
+insert into Customers (Id, Name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (Id, CustomerId) values ('1', '3');
+insert into Orders (Id, CustomerId) values ('2', '1');
+
+-- SELECT a.Name as Customers FROM Customers a WHERE a.Id NOT IN (SELECT CustomerId FROM Orders);  -- slower then below
+SELECT a.Name as Customers FROM Customers a left join Orders b ON a.Id=b.CustomerId WHERE b.Id IS NULL; 

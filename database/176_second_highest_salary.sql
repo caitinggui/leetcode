32976a29bfbb61621f05a9165fd3de229460bb11
caitinggui <1029645297@qq.com>
2017-12-16 16:48:42 +0800

    limit, distinct and order

---
+++ database/176_second_highest_salary.sql
@@ -0,0 +1,26 @@
+-- Write a SQL query to get the second highest salary from the Employee table.
+
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+
+reate table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (Id, Salary) values ('1', '100')
+insert into Employee (Id, Salary) values ('2', '200')
+insert into Employee (Id, Salary) values ('3', '300')
+
+-- 会报错，因为为空时没有返回null
+-- select distinct salary as SecondHighestSalary from Employee order by salary desc limit 1, 1
+select (select distinct salary from Employee order by salary desc limit 1, 1) as SecondHighestSalary;

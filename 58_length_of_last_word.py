fe589fd067076d47e5227f32c86e708768d8d230
caitinggui <1029645297@qq.com>
2018-02-27 09:16:29 +0800

    split

---
+++ 58_length_of_last_word.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+'''
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+
+'''
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        if not s:
+            return 0
+        return len(s.split(' ')[-1])
+
+
+s = Solution()
+st = "Hello World"
+print(s.lengthOfLastWord(st), 5)
+st = 'a '
+print(s.lengthOfLastWord(st), 1)
+# split

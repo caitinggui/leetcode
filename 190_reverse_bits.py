12b6ab09a68ddc38e89cf00df71c1e5913ff8aaf
caitinggui <1029645297@qq.com>
2018-03-05 18:34:33 +0800

    bin和int

---
+++ 190_reverse_bits.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+'''
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+Follow up:
+    If this function is called many times, how would you optimize it?
+'''
+
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        tmp = bin(n)[2:]
+        tmp = '0' * (32 - len(tmp)) + tmp
+        print(tmp)
+        return int(tmp[::-1], base=2)
+
+
+s = Solution()
+print(s.reverseBits(43261596), 964176192)
